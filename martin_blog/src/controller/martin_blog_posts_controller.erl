641bfb29be8fcb4735b0d0d77976cbda63551902
Martin Rosas <martinrosas84@gmail.com>
2017-02-26 20:36:46 -0300

    added changes

--- martin_blog/src/controller/martin_blog_posts_controller.erl
+++ martin_blog/src/controller/martin_blog_posts_controller.erl
@@ -5,14 +5,27 @@
 -compile(export_all).
 
 category('GET', [Category_id]) ->
+%% 	CUALQUIERA!!!
+	[_, IId] = string:tokens(Category_id, "-"),
+	Category = boss_db:find(Category_id),
 	Categories = boss_db:find(category, []),
-	erlang:display("entreeeeee"),
-	Posts_resp = case boss_db:find(post, [{category_id, 'equals', Category_id}]) of
+	Posts_resp = case boss_db:find(post, [{category_id, 'equals', IId}]) of
 					{error, Error} -> [];
 					Posts -> Posts
 				end,
-	{ok, [{categories, Categories}, {posts, Posts_resp}]}.
+	{ok, [{categories, Categories}, {posts, Posts_resp}, {category, Category}]}.
 %% 	case boss_db:find(post, [{category_id, 'equals', Category_id}]) of
 %% 		{error, Error} -> {ok, [{categories, Categories}, {posts, []}]};
 %% 		Posts -> {ok, [{categories, Categories}, {posts, Posts}]}
 %% 	end.
+
+view('GET', [Post_id]) ->
+	Categories = boss_db:find(category, []),
+	Post_resp = try case boss_db:find(Post_id) of
+						{error, Error} -> [];
+						Post -> Post
+				end
+			catch
+				_:_ -> []
+			end,
+	{ok, [{categories, Categories}, {post, Post_resp}]}.
--- martin_blog/src/model/post.erl
+++ martin_blog/src/model/post.erl
@@ -1,5 +1,5 @@
 %% @author martinrosas
 %% @doc @todo Add description to post.
 
--module(post, [Id, Title, Content, Introduction, Category_id, Pinned, Created, Updated]).
+-module(post, [Id::serial(), Title, Content, Introduction, Category_id, Pinned, Created, Updated]).
 -compile(export_all).
